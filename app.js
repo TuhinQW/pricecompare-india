4cecaf00ef93fd2929e0fb27220dc65c2b1c00d3
TuhinQW <tuhinc08787@gmail.com>
2026-09-14 18:51:25 +0530

    Update Supabase URL in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_URL = "https://vssthkgpibacnxczrlgp.supabase.co";
 const SUPABASE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
 
 const products = [
